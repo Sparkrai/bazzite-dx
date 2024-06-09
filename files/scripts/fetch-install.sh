f8dde0809a988ed021b4be2d7f2e6baec9584ea5
Sparkrai <sparkraiy@gmail.com>
2024-06-09 13:30:15 -0400

    Moved bluefin to files to use its files

---
+++ files/scripts/fetch-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+set -ouex pipefail
+
+# Starship Shell Prompt
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
+
+# Topgrade Install
+pip install --prefix=/usr topgrade
